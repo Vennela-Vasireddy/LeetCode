406dc4b8d835b54dc725dac543080e40fd88e217
Vennela-Vasireddy <64094167+Vennela-Vasireddy@users.noreply.github.com>
2024-01-23 15:32:24 -0800

    Time: 55 ms (87.14%), Space: 17.1 MB (90.16%) - LeetHub

---
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.py
@@ -0,0 +1,25 @@
+from itertools import permutations 
+
+class Solution:
+    def minPartitions(self, n: str) -> int:
+        return int(max(n))
+        
+              
+ 
+ 
+#printing all the binary numbers using combinations
+#         deci_binary_list = []
+#         for length in range(2, len(n)+1):
+#             perm = combinations_with_replacement ([1, 0], length) 
+#             for i in list(perm):
+#                 perm_str = ""
+#                 for x in i:
+#                     perm_str += str(x)
+#                 deci_binary_list.append(perm_str)
+#         print(deci_binary_list)
+                
+            
+            
+                
+            
+            
