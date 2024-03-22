c6003fa30d7ea6c260c6736642b34964ecc1d2b0
Vennela-Vasireddy <64094167+Vennela-Vasireddy@users.noreply.github.com>
2024-03-21 17:35:16 -0700

    Time: 108 ms (74.32%), Space: 17.9 MB (30.17%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,15 +1,7 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        if(len(nums) < 3):
-            if 0 not in nums:
-                return 0
-            if 1 not in nums:
-                return 1
-            if 2 not in nums:
-                return 2
+        if(max(nums)==len(nums)):
+            return int(((max(nums)*(max(nums)+1))/2) - sum(nums))
         else:
-            if(max(nums)==len(nums)):
-                return int(((max(nums)*(max(nums)+1))/2) - sum(nums))
-            else:
-                return len(nums)
+            return len(nums)
         
