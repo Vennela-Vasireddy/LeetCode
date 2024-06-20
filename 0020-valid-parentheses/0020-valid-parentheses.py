a52003f445e8ef8b2c8cc3a335baaa965e575fcd
Vennela-Vasireddy <64094167+Vennela-Vasireddy@users.noreply.github.com>
2024-06-19 17:22:58 -0700

    Time: 33 ms (73.73%), Space: 16.5 MB (90.84%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,28 +1,32 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-#         dictionary = { ')':'(' ,'}':'{', ']' : '[' }
-#         stack = []
-#         for x in range(0,len(s)):
-#             if s[x] in ['(','{', '[']:
-#                 stack.append(s[x])
-#             else:
-#                 if len(stack) == 0 : return False
-
-#                 if stack.pop()!= dictionary[s[x]]:
-#                     return False
-#             return True
-        
-        stack = []
-        mapping = {
+        dictionary = {
             ")": "(", 
             "]": "[", 
             "}": "{"
         }
+        stack = []
         for i in s:
             if i in ["(", "[", "{"]:
                 stack.append(i)
             else:
                 if not stack: return False
-                if stack.pop() != mapping[i]:
+                if stack.pop() != dictionary[i]:
                     return False
-        return not stack
+        return len(stack)==0
+        
+        
+        # stack = []
+        # mapping = {
+        #     ")": "(", 
+        #     "]": "[", 
+        #     "}": "{"
+        # }
+        # for i in s:
+        #     if i in ["(", "[", "{"]:
+        #         stack.append(i)
+        #     else:
+        #         if not stack: return False
+        #         if stack.pop() != mapping[i]:
+        #             return False
+        # return not stack
