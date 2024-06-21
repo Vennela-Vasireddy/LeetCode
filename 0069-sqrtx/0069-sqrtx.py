d3e85c48ca7915950be734ff994d6dfb949f81bc
Vennela-Vasireddy <64094167+Vennela-Vasireddy@users.noreply.github.com>
2024-06-21 12:20:25 -0700

    Time: 46 ms (31.92%), Space: 16.5 MB (27.22%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -15,30 +15,6 @@ class Solution:
             else:
                 i = mid
         return i
-                
-            # import math
-
-# class Solution:
-#     def mySqrt(self, x: int) -> int:
-#         start = 1
-#         end = x
-#         mid = -1
-
-#         if x == 0:
-#             return 0
-
-#         while start < end - 1:
-#             mid = math.floor(start + 0.5 * (end - start))
-#             mid_sq = mid * mid
-
-#             if mid_sq > x:
-#                 end = mid
-#             elif mid_sq == x:
-#                 return mid  # for perfect squares
-#             else:
-#                 start = mid
-
-#         return start
 
     
             
