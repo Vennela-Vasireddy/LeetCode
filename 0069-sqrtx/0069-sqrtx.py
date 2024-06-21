6460391e7ce77795bac391c06aace709d3c5db4e
Vennela-Vasireddy <64094167+Vennela-Vasireddy@users.noreply.github.com>
2024-06-21 12:20:53 -0700

    Time: 45 ms (34.53%), Space: 16.6 MB (27.22%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -7,7 +7,7 @@ class Solution:
         if x == 0 or x==1:
             return x
         while(i<j-1):
-            mid = math.floor(i + 0.5 * (j - i))
+            mid = math.floor((i + j)/2)
             if(mid*mid > x):
                 j = mid
             elif( mid*mid == x ):
