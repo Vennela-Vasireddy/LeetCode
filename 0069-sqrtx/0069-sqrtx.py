7838a3bdff89534d42a74fb78e2fb0a7af68c9a6
Vennela-Vasireddy <64094167+Vennela-Vasireddy@users.noreply.github.com>
2024-06-21 12:20:04 -0700

    Time: 44 ms (37.64%), Space: 16.5 MB (72.85%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,43 +1,44 @@
-# import math
-# class Solution:
-#     def mySqrt(self, x: int) -> int:
-#         i = 1
-#         j = x
-#         if x == 0 or x==1:
-#             return x
-#         while(i<j):
-#             mid = math.floor((i + j)/2)
-#             if(mid*mid == x):
-#                 return int(mid)
-#             elif( mid*mid > x ):
-#                 j = mid
-#             else:
-#                 i = mid
-#         return i
-                
 import math
-
 class Solution:
     def mySqrt(self, x: int) -> int:
-        start = 1
-        end = x
+        i = 1
+        j = x
         mid = -1
-
-        if x == 0:
-            return 0
-
-        while start < end - 1:
-            mid = math.floor(start + 0.5 * (end - start))
-            mid_sq = mid * mid
-
-            if mid_sq > x:
-                end = mid
-            elif mid_sq == x:
-                return mid  # for perfect squares
+        if x == 0 or x==1:
+            return x
+        while(i<j-1):
+            mid = math.floor(i + 0.5 * (j - i))
+            if(mid*mid > x):
+                j = mid
+            elif( mid*mid == x ):
+                return int(mid)
             else:
-                start = mid
+                i = mid
+        return i
+                
+            # import math
 
-        return start
+# class Solution:
+#     def mySqrt(self, x: int) -> int:
+#         start = 1
+#         end = x
+#         mid = -1
+
+#         if x == 0:
+#             return 0
+
+#         while start < end - 1:
+#             mid = math.floor(start + 0.5 * (end - start))
+#             mid_sq = mid * mid
+
+#             if mid_sq > x:
+#                 end = mid
+#             elif mid_sq == x:
+#                 return mid  # for perfect squares
+#             else:
+#                 start = mid
+
+#         return start
 
     
             
