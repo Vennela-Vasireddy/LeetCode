e12f85b1b5deec6b6d3c172971bbc69d443442cf
Vennela Vasireddy <64094167+Vennela-Vasireddy@users.noreply.github.com>
2024-10-03 15:46:51 -0700

    Time: 421 ms (16.19%), Space: 16.6 MB (73.12%) - LeetHub

---
+++ 0060-permutation-sequence/0060-permutation-sequence.py
@@ -0,0 +1,17 @@
+from itertools import permutations 
+class Solution:
+    def getPermutation(self, n: int, k: int) -> str:
+        count = 0
+        string = ''
+        result = []
+        sequence = [x for x in range(1, n+1)]
+        perm = permutations(sequence)
+        for x in perm:
+            count = count+1
+            if(count == k):
+                result = x
+                break
+        return ''.join(map(str, result))
+                
+            
+        
