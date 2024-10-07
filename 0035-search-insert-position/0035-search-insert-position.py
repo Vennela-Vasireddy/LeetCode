414557259c0f7407ba391e828242e48706629364
Vennela Vasireddy <64094167+Vennela-Vasireddy@users.noreply.github.com>
2024-10-07 15:25:38 -0700

    Time: 49 ms (57.99%), Space: 17.3 MB (48.23%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,28 @@
+def binarySearch(self, nums, initial, end, target):
+    if end>=initial:
+        mid = initial + (end - initial)//2
+        if(nums[mid] == target):
+            return mid
+        elif(nums[mid] > target):
+            return binarySearch(self,nums,initial ,mid-1, target)
+            
+        else:
+            return binarySearch(self,nums, mid+1, end, target)
+    else:
+        return initial
+        
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        intial = 0 
+        end = len(nums)-1
+        value = binarySearch(self,nums, intial, end, target)
+        return value
+
+    
+            
+            
+        
+        
+        
+        
