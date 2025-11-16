17b6852bf9dfd4ee61da5be68c19f6c70a052c9a
Vennela Vasireddy <64094167+Vennela-Vasireddy@users.noreply.github.com>
2025-11-15 18:24:22 -0800

    Create 2519-FindTheOriginalArrayOfPrefixXor

---
+++ 2519-FindTheOriginalArrayOfPrefixXor/2519-FindTheOriginalArrayOfPrefixXor.py
@@ -0,0 +1,13 @@
+# Last updated: 11/15/2025, 6:24:22 PM
+class Solution:
+    def findArray(self, pref: List[int]) -> List[int]:
+        arr = []
+        arr.append(pref[0])
+        for x in range(0, len(pref)-1):
+            arr.append(pref[x+1]^pref[x])
+        return arr
+            
+            
+            
+        
+        
