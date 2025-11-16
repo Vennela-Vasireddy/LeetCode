4745894992d7614f52cba61833a812bd998e4255
Vennela Vasireddy <64094167+Vennela-Vasireddy@users.noreply.github.com>
2025-11-15 18:24:26 -0800

    Create 2271-RearrangeArrayElementsBySign

---
+++ 2271-RearrangeArrayElementsBySign/2271-RearrangeArrayElementsBySign.py
@@ -0,0 +1,18 @@
+# Last updated: 11/15/2025, 6:24:26 PM
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        even_index, odd_index = 0,1
+        result = [0]*len(nums)
+        for x in nums:
+            if x>0:
+                result[even_index] = x
+                even_index +=2
+            else:
+                result[odd_index] = x
+                odd_index +=2
+        return result
+                
+        
+        
+            
+        
