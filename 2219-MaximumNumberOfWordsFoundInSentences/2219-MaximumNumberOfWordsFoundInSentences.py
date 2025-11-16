ecf378369e22af7086a62349b599d77f0a3e98e6
Vennela Vasireddy <64094167+Vennela-Vasireddy@users.noreply.github.com>
2025-11-15 18:24:27 -0800

    Create 2219-MaximumNumberOfWordsFoundInSentences

---
+++ 2219-MaximumNumberOfWordsFoundInSentences/2219-MaximumNumberOfWordsFoundInSentences.py
@@ -0,0 +1,19 @@
+# Last updated: 11/15/2025, 6:24:27 PM
+class Solution:
+    def mostWordsFound(self, sentences: List[str]) -> int:
+        # Traverse through each sentence and find out the words by checking whether the literal == " " and increase count and return the max count - This is a bruteforce approach
+        
+        max_count = 0
+        for sentence in sentences:
+            current_count = 0
+            for literal in sentence:
+                if literal == " ":
+                    current_count = current_count + 1
+            max_count = max(current_count, max_count)
+        return max_count+1
+                
+            
+            
+            
+        
+        
